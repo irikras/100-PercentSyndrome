bab993ebecb3dc0c1f18b93f5d8cd27b14020bc2
irikras <irikras@mail.ru>
2021-08-01 15:13:59 +0300

    Added failIfNoTests

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,3 +1,4 @@
+
 package ru.netology.statistic;
 
 import org.junit.jupiter.api.Test;
@@ -6,15 +7,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StatisticsServiceTest {
 
-  @Test
-  void findMax() {
-    StatisticsService service = new StatisticsService();
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
 
-    long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-    long expected = 12;
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
 
-    long actual = service.findMax(incomesInBillions);
+        long actual = service.findMax(incomesInBillions);
 
-    assertEquals(expected, actual);
-  }
+        assertEquals(expected, actual);
+    }
 }
